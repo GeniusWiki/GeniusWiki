bfa65dc3dc48e7a5ad3030e70cecaa1636a43a2c
Dapeng Ni <dapeng.ni@edgenius.com>
2012-06-28 14:55:47 +1000

    import

--- src/wiki/java/com/edgenius/wiki/service/impl/SitemapServiceImpl.java
+++ src/wiki/java/com/edgenius/wiki/service/impl/SitemapServiceImpl.java
@@ -37,8 +37,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
